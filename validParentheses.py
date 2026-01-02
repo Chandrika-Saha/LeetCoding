79070848c82c5738dc1972fb60290bc28bd2a055
Chandrika Saha <chandrika.cse1.bu@gmail.com>
2026-01-02 15:11:48 -0500

    Valid Parentheses good, efficient

--- validParentheses.py
+++ validParentheses.py
@@ -1,18 +1,37 @@
 class Solution:
+
     def isValid(self, s: str) -> bool:
 
-        stack = []
-
-        bracketMap = {')' : '(', '}' : '{', ']' : '['}
+        m = {")": "(", "}": "{", "]": "["}
+        st = set(m.values())
+        l =[]
 
         for c in s:
-            if c in bracketMap:
-                if stack and stack[-1] == bracketMap[c]:
-                    stack.pop()
-                else:
-                   return False 
-            else:
-                stack.append(c)
+            if c in st:
+                l.append(c)
+            if c in m:
+                if not l:
+                    return False
+                if m[c] != l[-1]:
+                    return False
+                l.pop()
 
-        return True if not stack else False
+        return not l
+
+#     def isValid(self, s: str) -> bool:
+# 
+#         stack = []
+# 
+#         bracketMap = {')' : '(', '}' : '{', ']' : '['}
+# 
+#         for c in s:
+#             if c in bracketMap:
+#                 if stack and stack[-1] == bracketMap[c]:
+#                     stack.pop()
+#                 else:
+#                    return False 
+#             else:
+#                 stack.append(c)
+# 
+#         return True if not stack else False
         
