640dbbeb210bb50bb99f70a0bad4a9abc22dbf1b
Chandrika Saha <chandrika.cse1.bu@gmail.com>
2026-01-18 18:58:42 -0500

    threading

---
+++ MultitreadingAndMultiprocessing/multi_threading.py
@@ -0,0 +1,46 @@
+import threading
+import time
+
+
+def calc_square(nums):
+    print("Calculating square numbers")
+    for n in nums:
+        time.sleep(1)
+        print("Square:", n)
+
+def calc_cube(nums):
+    print("Calculate cube of numbers")
+    for n in nums:
+        time.sleep(1)
+        print("Cube:", n)
+
+arr = [2, 3, 4, 5]
+
+# Multi-treading in Python follow the GIL (Global Interpreter Lock), that make the interpreter use only
+# one CPU until am I/O thing is prompted. So, we use multi-threading for I/O bound load and
+# multiprocessing for CPU bound loads.
+
+t = time.time()
+
+calc_square(arr)
+calc_cube(arr)
+
+print("Done in: ", time.time() - t)
+
+tt = time.time()
+
+t1 = threading.Thread(target=calc_square, args=(arr,))
+t2 = threading.Thread(target=calc_cube, args=(arr,))
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
+
+print("Threading done in: ", time.time()-tt)
+
+
+
+
+
